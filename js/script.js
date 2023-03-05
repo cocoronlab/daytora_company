dfd76db05efd6fffab4106ccdfa26a79d1b40675
cocoronlab <cocoronlab@gmail.com>
2023-03-05 19:39:26 +0900

    スムーススクロール実装完了

--- js/script.js
+++ js/script.js
@@ -27,3 +27,17 @@ jQuery('.drawer-icon').on('click', function(e) {
 
   return false;
 });
+
+jQuery('a[href^="#"]').on('click', function() {
+
+  var header = jQuery('.header').innerHeight();
+  var id = jQuery(this).attr('href');
+  var position = 0;
+  if ( id != '#') {
+    var position = jQuery(id).offset().top - header;
+  }
+  jQuery('html,body').animate({
+    scrollTop: position
+  },
+  300);
+});
